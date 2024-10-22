f457f20461743ba1d575191d9df89ffea7a791f8
Dark Madani <darkmadani@Gmail.com>
2024-10-22 13:18:53 +0700

    Full detail

--- src/data/project.js
+++ src/data/project.js
@@ -3,26 +3,27 @@ import calculator from "../assets/projects/calculator.png";
 import login from "../assets/projects/login.png";
 import dashboard from "../assets/projects/dashboard.png";
 
+
 const projects = [
   {
     name: "Calculator",
     img: calculator,
     description:
-      "",
+      "Tugas tentang Kalkulator",
     link: "calculator.html",
   },
   {
     name: "Form Login / Register",
     img: login,
     description:
-      "",
+      "Tugas tentang Form Login dan Register",
     link: "login.html",
   },
   {
     name: "Dashboard",
     img: dashboard,
     description:
-      "",
+      "Tugas tentang Dashboard",
     link: "/dashboard/index.html",
   },
 ];
