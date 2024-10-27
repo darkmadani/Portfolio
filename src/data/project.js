f38fca162d5bb2c599994724695e1129e060ad10
Dark Madani <darkmadani@gmail.com>
2024-10-28 00:10:48 +0700

    penambahan javascript intermediate

--- src/data/project.js
+++ src/data/project.js
@@ -10,21 +10,21 @@ const projects = [
     name: "Calculator",
     img: calculator,
     description:
-      "Tugas tentang Kalkulator",
+      "",
     link: "/calculator.html",
   },
   {
     name: "Form Login / Register",
     img: login,
     description:
-      "Tugas tentang Form Login dan Register",
+      "",
     link: "/login.html",
   },
   {
     name: "Dashboard",
     img: dashboard,
     description:
-      "Tugas tentang Dashboard",
+      "",
     link: "/dashboard/index.html",
   },
 ];
